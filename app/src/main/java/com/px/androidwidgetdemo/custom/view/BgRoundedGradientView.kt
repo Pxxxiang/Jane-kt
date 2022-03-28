a2d09661709550b3983bbbe29495a008e0048aba
panxiang <panxiang@babybus.com>
2022-03-28 09:22:42 +0800

    初始化项目

---
+++ app/src/main/java/com/px/androidwidgetdemo/custom/view/BgRoundedGradientView.kt
@@ -0,0 +1,17 @@
+package com.px.androidwidgetdemo.custom.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * 自定义圆角渐变View
+ *
+ * @author panxxx
+ * @date 2022-03-28
+ */
+class BgRoundedGradientView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+}
